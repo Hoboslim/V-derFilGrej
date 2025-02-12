bec6f3a51b07576e14a209681a33026e3babb7c9
Jojo Lindell <johannes152@hotmail.com>
2025-02-12 10:09:52 +0100

    början på meny

--- VäderFilGrej/Program.cs
+++ VäderFilGrej/Program.cs
@@ -6,21 +6,74 @@ namespace VäderFilGrej
     {
         static void Main(string[] args)
         {
-            //ReadFiles.ReadFiles.ReadFile();
-            //ExtractInformation.MedelTemp.ExtractMedelTemp();
-            //ExtractInformation.WarmestColdest.WarmCold();
-            var data = new GetData();
             var saveData = new SaveData();
 
-            //data.WarmCold();
-            //data.MedTemp();
-            //data.Mold();
-            //data.DryWet();
-            //data.Fall();
-            //data.vinter();
+
             //saveData.tempList();
-            data.Search();
+            while (true)
+            {
+
+                Console.WriteLine("Vill du läsa eller spara data?");
+                Console.WriteLine("Tryck 1 för att läsa");
+                Console.WriteLine("Tryck 2 för att spara");
+                Console.WriteLine("Tryck Esc för att avsluta");
+
+                var key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        ReadMenu();
+                        break;
+
+                    case ConsoleKey.D2:
+                        break;
+
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            void ReadMenu()
+            {
+                var data = new GetData();
+                Console.WriteLine("Vilken data vill du se?");
+
+
+                var key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        data.WarmCold();
+                        break;
+
+                    case ConsoleKey.D2:
+                        data.MedTemp();
+                        break;
+                    case ConsoleKey.D3:
+                        data.Mold();
+                        break;
+                    case ConsoleKey.D4:
+                        data.DryWet();
+                        break;
+                    case ConsoleKey.D5:
+                        data.Fall();
+                        break;
+                    case ConsoleKey.D6:
+                        data.vinter();
+                        break;
+                    case ConsoleKey.D7:
+                        data.Search();
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                }
+            }
 
         }
+
+
+        
     }
 }
